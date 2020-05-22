458504f868e4a8f7526a9d35d05ea9ed73788c12
palaktank <101913785@student.swin.edu.au>
2020-05-22 13:09:49 +1000

    Updates

---
+++ precache-manifest.b2e3666e36f8d594d7ab38fddfe240c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321e26072fee03155829312223bfea3b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fee19c1fd97829676cdf",
+    "url": "/monsters-rolodex/static/css/main.7de08103.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee19c1fd97829676cdf",
+    "url": "/monsters-rolodex/static/js/main.74400c7c.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
